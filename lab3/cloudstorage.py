603c3f3c2b3728970baf2b9370345c23184b9e9c
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-08-26 09:33:18 +0800

    finishes upload component

--- lab3/cloudstorage.py
+++ lab3/cloudstorage.py
@@ -17,9 +17,6 @@ ROOT_S3_DIR = '22487668-cloudstorage'
 SHORT_ARGS = "ih"
 LONG_ARGS = ["initialise", "help"]
 
-
-s3 = boto3.client("s3")
-
 bucket_config = {'LocationConstraint': 'ap-southeast-2'}
 
 def upload_file(folder_name, file, file_name):
@@ -27,28 +24,56 @@ def upload_file(folder_name, file, file_name):
     print("Uploading %s" % file)
 
 
-# Main program
-# Insert code to create bucket if not there
+def print_help():
+    print(
+        f"Usage cloudstorage.py [OPTION]\n"
+        f"Uploads the files in a given directory to S3\n"
+        f"-i, --initialise\tCreate a new S3 bucket\n"
+        f"-h, --help\tDisplay this help menu then quit\n"
+    )
+
 
 def main():
     opts, args = getopt.getopt(sys.argv[1:], SHORT_ARGS, LONG_ARGS)
-    print(opts, args)
+    initialise = False
+    s3_client = boto3.client("s3")
+    s3_resource = boto3.resource("s3")
+    for opt in opts:
+        if opt[0] == '-i' or opt[0] == '--initalise':
+            initialise = True
+        elif opt[0] == '-h' or opt[0] == '--help':
+            print_help()
+            return
 
-    # try:
+    try:
+        if initialise:
+            s3_client.create_bucket(
+                ACL='private',
+                Bucket=ROOT_S3_DIR,
+                CreateBucketConfiguration=bucket_config
+            )
+    except s3_client.exceptions.BucketAlreadyExists:
+        print(f"Bucket {ROOT_S3_DIR} already exists")
+        return
+    except s3_client.exceptions.BucketAlreadyOwnedByYou:
+        print(f"Bucket {ROOT_S3_DIR} has already been created by you")
+        return
 
-    #     print(response)
-    # except Exception as error:
-    #     pass
 
+    # parse directory and upload files
 
-    # # parse directory and upload files
-
-    # for dir_name, subdir_list, file_list in os.walk(ROOT_DIR, topdown=True):
-    #     if dir_name != ROOT_DIR:
-    #         for fname in file_list:
-    #             upload_file("%s/" % dir_name[2:], "%s/%s" % (dir_name, fname), fname)
-
-
+    for dir_name, subdir_list, file_list in os.walk(ROOT_DIR, topdown=True):
+        if dir_name != ROOT_DIR:
+            for fname in file_list:
+                print(
+                    f"{dir_name[2:]}/{fname}"
+                )
+                path = f"{dir_name[2:]}/{fname}"
+                s3_resource.meta.client.upload_file(
+                    Filename=path,
+                    Bucket=ROOT_S3_DIR,
+                    Key=path
+                )
     print("done")
 
 if __name__ == "__main__":
---
+++ lab3/restorefromcloud.py
@@ -0,0 +1,15 @@
+#! /usr/env/bin python3
+"""
+__author__ = "Eddie Atkinson"
+__copyright__ = "Copyright 2020"
+
+Simple script to download files and folders from S3 preserving the folder structure
+"""
+import boto3
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
