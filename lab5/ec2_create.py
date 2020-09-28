eadeb8ed5fb67022bd4e1fa114c7005669b3122e
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-09-28 12:13:48 +0800

    finishes lab5

--- lab5/ec2_create.py
+++ lab5/ec2_create.py
@@ -6,15 +6,14 @@ AZs and removing them
 __author__ = "Eddie Atkinson"
 __copyright__ = "Copyright 2020"
 """
-import boto3
-from namesgenerator import get_random_name
 import pprint
-from time import sleep
+import boto3
 
+# Set your keyname, security group name and VPC ID below
 
-KEY_NAME = "22487668-key"
-SG_NAME = "22487668-sg"
-VPC_ID = "vpc-03c9fef49916d386a"
+KEY_NAME = ""
+SG_NAME = ""
+VPC_ID = ""
 
 
 def stop_instances(ec2_client, instance_ids):
@@ -45,7 +44,7 @@ def create_instances(ec2_resource, az):
     return instance_ids
 
 
-def get_subnet_ids(ec2_client, ids):
+def get_subnet_ids(ec2_client, instance_ids):
     subnet_ids = []
     instances = ec2_client.describe_instances(
         InstanceIds=instance_ids
@@ -61,39 +60,73 @@ def get_sg_id(ec2_client):
         Filters=[
             {
                 "Name": "group-name",
-                "Values": SG_NAME
+                "Values": [SG_NAME]
             }
         ]
-    )
-    print(resp)
+    )["SecurityGroups"]
+    if len(resp) == 0:
+        return None
+    else:
+        return resp[0]["GroupId"]
 
-def create_elb(elb, subnet_ids):
+
+def create_elb(elb, subnet_ids, sg_id):
     resp = elb.create_load_balancer(
         Name="22487668-lb",
         Subnets=subnet_ids,
-        SecurityGroups=[SG_NAME],
+        SecurityGroups=[sg_id],
+    )["LoadBalancers"][0]
+    return resp["LoadBalancerArn"]
+
+
+def set_target_group(elb, instance_ids):
+    resp = elb.create_target_group(
+        Name="22487668-targetgroup",
+        Protocol="HTTP",
+        Port=80,
+        VpcId=VPC_ID
+    )["TargetGroups"]
+    tg_arn = resp[0]["TargetGroupArn"]
+    targets = [{"Id": id} for id in instance_ids]
+    elb.register_targets(
+        TargetGroupArn=tg_arn,
+        Targets=targets
+    )
+    return tg_arn
+
+
+def attach_listener(elb, elb_arn, tg_arn):
+    resp = elb.create_listener(
+        LoadBalancerArn=elb_arn,
+        Protocol="HTTP",
+        Port=80,
+        DefaultActions=[{
+            "Type": "forward",
+            "TargetGroupArn": tg_arn
+        }]
     )
     pprint.pprint(resp)
 
 
-ec2_resource = boto3.resource("ec2")
-ec2_client = boto3.client("ec2")
-elbv2 = boto3.client("elbv2")
+def main():
+    ec2_resource = boto3.resource("ec2")
+    ec2_client = boto3.client("ec2")
+    elbv2 = boto3.client("elbv2")
+
+    instance_ids = []
+    instance_ids.append(*create_instances(ec2_resource, "c"))
+    instance_ids.append(*create_instances(ec2_resource, "b"))
+    subnet_ids = get_subnet_ids(ec2_client, instance_ids)
+    try:
+        sg_id = get_sg_id(ec2_client)
+        elb_arn = create_elb(elbv2, subnet_ids, sg_id)
+        tg_arn = set_target_group(elbv2, instance_ids)
+        attach_listener(elbv2, elb_arn, tg_arn)
+    except Exception as e:
+        print(e)
+        stop_instances(ec2_client, instance_ids)
 
 
-# instance_ids = []
-# instance_ids.append(*create_instances(ec2_resource, "c"))
-# instance_ids.append(*create_instances(ec2_resource, "b"))
-# subnet_ids = get_subnet_ids(ec2_client, instance_ids)
-# print(subnet_ids)
-# try:
-#     create_elb(elbv2, subnet_ids)
-#     stop_instances(ec2_client, instance_ids)
-# except Exception as e:
-#     print(e)
-#     stop_instances(ec2_client, instance_ids)
-
-get_sg_id(ec2_client)
-
-
+if __name__ == "__main__":
+    main()
 
